dcc0ba9f5dfe058bb927b8327bd851d3ca527719
Hamza M <hamza-x@users.noreply.github.com>
2026-01-06 13:42:23 +0000

    remove HMX menus and dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Form from "./components/Form";
-import { useMediaQuery } from "@mui/material";
-import { NavBar } from "@hmxlabs/navbar";
 import "./index.css";
 
 const App = () => {
-  const [mobileOpen, setMobileOpen] = useState(false);
-
-  const isMobile = useMediaQuery("(max-width:768px)");
-
   return (
-    <>
-      <NavBar
-        isMobile={isMobile}
-        mobileOpen={mobileOpen}
-        onToggleMobileMenu={() => setMobileOpen((prev) => !prev)}
-      />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Form />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Form />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
